e65a34fc6030dbf2d86d36af1e7b01e0400c570e
Glexe <gl.pvn12@gmail.com>
2021-06-07 17:42:52 +0200

    Remade stock entity

--- StockPortfolio.Shared/Models/Stock.cs
+++ StockPortfolio.Shared/Models/Stock.cs
@@ -1,34 +1,20 @@
-﻿using StockPortfolio.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace StockPortfolio.Models
 {
-    public class Stock
+    public class Stock : EntityBase
     {
-        public Stock()
-        {
-            User_Stocks = new HashSet<User_Stock>();
-        }
-
         public string Symbol { get; set; }
 
+        public int StockID { get; set; }
+        public string Symbol { get; set; }
         public string Name { get; set; }
-
         public decimal Price { get; set; }
-
         public decimal DayChange { get; set; }
-
         public decimal ChangePercentage { get; set; }
-
-        public decimal Volume { get; set; }
-
         public decimal VolumeAvg { get; set; }
-
         public decimal MarketCap { get; set; }
-
         public decimal? PricePerEarningRatio { get; set; }
-
-        public bool IsCategorized { get; set; }
 
         public ICollection<User_Stock> User_Stocks { get; set; }
     }
